9a7eabe93730be2acebd89ee69b76000ff4080d0
stepan <79817353737@yandex.ru>
2024-06-11 18:18:22 +0300

    add categories in storage & fix code

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 const $calendar = document.getElementById('calendar');
 const $totalExpences = document.getElementById('total-expences');
 const $categoriesContainer = document.getElementById('categories');
-const $categories = document.querySelectorAll('#categories div')
 const $title = document.getElementById('title');
 const $list = document.getElementById('list');
 const $selectedCategory = document.getElementById('selected-category');
@@ -14,14 +13,54 @@ const $comment = document.getElementById('comment');
 const $submit = document.getElementById('submit');
 
 /**
+ * Data
+ */
+
+const categories = [
+    "Продукты",
+    "Дом",
+    "Одежда",
+    "Здоровье",
+    "Спорт",
+    "Кафе",
+    "Поездки",
+    "Авто",
+    "Развлечения",
+    "Обучение",
+    "% по кредитам",
+    "Другое"
+];
+
+/**
  * Main
  */
 
-$categories.forEach($category => {
-    $category.addEventListener('click', () => {
-        const selectedCategory = $category.innerText;
-        localStorage.setItem('selectedCategory', selectedCategory);
-        window.location.href = './form of creating expense.html';
+// Сохранения категорий в localStorage
+
+localStorage.setItem('expenseCategories', JSON.stringify(categories));
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Заполнение categoriesContainer
+    let categories = JSON.parse(localStorage.getItem('expenseCategories'));
+
+    $categoriesContainer.innerHTML = '';
+    categories.forEach(category => {
+        const $categoryDiv = document.createElement('div');
+        $categoryDiv.innerText = category;
+        $categoryDiv.classList.add('category');
+        $categoriesContainer.appendChild($categoryDiv);
+    });
+
+    // Добавление обработчиков событий для категорий
+    const $categories = document.querySelectorAll('.category');
+    
+    $categories.forEach($category => {
+        $category.addEventListener('click', () => {
+            const selectedCategory = $category.innerText;
+            localStorage.setItem('selectedCategory', selectedCategory);
+            window.location.href = './form of creating expense.html';
+        });
     });
 });
 
