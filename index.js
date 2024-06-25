1b507e5f073e6398a1066d18922a94af9c23ba47
stepan <79817353737@yandex.ru>
2024-06-25 21:53:18 +0300

    add expenses API

--- index.js
+++ index.js
@@ -5,10 +5,11 @@
 import Pager from './lib/Pager.js'
 import { categoriesList } from './data/categoriesList.js' 
 import pagerConfig from './configs/pager.js'
-import expensesStorage from './lib/expensesStorage.js';
-import { categoriesStorage, createCategoryElement, fillCategoriesContainer } from './lib/categories.js';
+import { expensesStorage, createExpenseElement, renderExpensesList } 
+    from './lib/expenses.js';
+import { categoriesStorage, createCategoryElement, fillCategoriesContainer }
+     from './lib/categories.js';
 import { getSelectedCategory } from './data/state.js'
-import { createDateElement } from './lib/dates.js';
 
 /**
  * Main
@@ -27,47 +28,6 @@ const $submit = document.getElementById('submit');
 /**
  * Functions
  */
-
-// Очистка контейнера
-
-const clearContainer = ($сontainer) => {
-    $сontainer.innerHTML = '';
-};
-
-// // Создание элемента даты
-// const createDateElement = (timestamp) => {
-//     const $dateDiv = document.createElement('div');
-//     $dateDiv.className = 'date';
-//     // преобразование даты в читабельный формат
-//     const dateObject = new Date(timestamp);
-//     $dateDiv.innerText = dateObject.toLocaleString(); 
-
-//     return $dateDiv;
-// };
-
-// Создание элемента расхода
-const createExpenseElement = (expense) => {
-    const $expenseDiv = document.createElement('div');
-    $expenseDiv.className = 'expense';
-    $expenseDiv.innerText = 
-        `Категория: ${expense.category}, Сумма: ${expense.sum}, `
-        + `Комментарий: ${expense.comment}`;
-
-    return $expenseDiv;
-};
-
-// Рендеринг списка расходов
-const renderExpensesList = ($list) => {
-    clearContainer($list);
-    const expenses = expensesStorage.getAll();
-
-    expenses.forEach(expense => {
-        const $dateDiv = createDateElement(expense.date);
-        const $expenseDiv = createExpenseElement(expense);
-        $dateDiv.appendChild($expenseDiv);
-        $list.appendChild($dateDiv);
-    });
-};
 
 // Обработка отправки формы
 const handleSubmit = () => {
