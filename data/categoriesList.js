de50028ab0604661745e94d9a2cfe9df8c0e03c7
stepan <79817353737@yandex.ru>
2024-08-13 20:58:41 +0300

    add icon classes

--- data/categoriesList.js
+++ data/categoriesList.js
@@ -3,18 +3,18 @@
  */
 
 const categoriesList = [
-    { id: 1, name: "Продукты" },
-    { id: 2, name: "Дом" },
-    { id: 3, name: "Одежда" },
-    { id: 4, name: "Здоровье" },
-    { id: 5, name: "Спорт" },
-    { id: 6, name: "Кафе" },
-    { id: 7, name: "Поездки" },
-    { id: 8, name: "Авто" },
-    { id: 9, name: "Развлечения" },
-    { id: 10, name: "Обучение" },
-    { id: 11, name: "% по кредитам" },
-    { id: 12, name: "Другое" }
+    { id: 1, name: "Продукты", iconClass: "bi bi-cart3" },
+    { id: 2, name: "Дом", iconClass: "bi bi-house-door"  },
+    { id: 3, name: "Одежда", iconClass: "bi bi-handbag"  },
+    { id: 4, name: "Здоровье", iconClass: "bi bi-capsule"  },
+    { id: 5, name: "Спорт", iconClass: "bi bi-bicycle"  },
+    { id: 6, name: "Кафе", iconClass: "fa fa-cutlery"  },
+    { id: 7, name: "Отдых", iconClass: "bi bi-luggage"  },
+    { id: 8, name: "Авто", iconClass: "bi bi-car-front"  },
+    { id: 9, name: "Развлечения", iconClass: "bi bi-joystick"  },
+    { id: 10, name: "Обучение", iconClass: "bi bi-book"  },
+    { id: 11, name: "% по кредитам", iconClass: "bi bi-percent"  },
+    { id: 12, name: "Другое", iconClass: "bi bi-card-list"  }
 ];
 
 /**
