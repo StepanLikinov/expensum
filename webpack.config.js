d5d64f2da30e587589f718a102668f05e5c0355b
stepan <79817353737@yandex.ru>
2024-10-02 13:08:34 +0300

    fix config

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
           template: './index.html',
-          favicon: 'favicon.ico'
+          favicon: './favicon.ico'
         }),
     ],
 };
