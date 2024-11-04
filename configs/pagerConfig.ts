b3b57a8b4908591722972ce38aab84066cd19dde
stepan <79817353737@yandex.ru>
2024-11-04 20:48:56 +0300

    ts configs

---
+++ configs/pagerConfig.ts
@@ -0,0 +1,42 @@
+/**
+ * Nodes
+ */
+
+const $main: HTMLElement | null = document.getElementById('main')
+const $new: HTMLElement | null = document.getElementById('new')
+const $list: HTMLElement | null = document.getElementById('list')
+const $mainLink: HTMLElement | null = document.getElementById('mainLink');
+const $newExpenseLink: HTMLElement | null = 
+    document.getElementById('newExpenseLink');
+const $expensesListLink: HTMLElement | null = 
+    document.getElementById('expensesListLink');
+
+/**
+ * Main
+ */
+
+interface PageConfig {
+    pageElement: HTMLElement | null;
+    linkToPage: HTMLElement | null;
+}
+
+const pagerConfig: { [key: string]: PageConfig } = {
+    'main': { 
+        pageElement: $main, 
+        linkToPage: $mainLink 
+    },
+    'new': { 
+        pageElement: $new, 
+        linkToPage: $newExpenseLink
+    },
+    'list': { 
+        pageElement: $list,
+        linkToPage: $expensesListLink
+    }
+}
+
+/**
+ * Exports 
+ */
+
+export default pagerConfig;
