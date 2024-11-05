e76b85dc8d82802816832e39f76a3cfcda4cd131
stepan <79817353737@yandex.ru>
2024-11-05 22:45:34 +0300

    ts navDomApi

---
+++ lib/navDomApi.ts
@@ -0,0 +1,68 @@
+/**
+ * Nodes
+ */
+
+const $mainLink: HTMLElement | null = document.getElementById('mainLink')
+const $newExpenseLink: HTMLElement | null = 
+    document.getElementById('newExpenseLink');
+const $expensesListLink: HTMLElement | null = 
+    document.getElementById('expensesListLink');
+const $expenseForm: HTMLElement | null = 
+    document.getElementById('expense-form');
+
+/**
+ * DOM API
+ */
+
+interface NavDomApi {
+    setActive: ($clickedLink: HTMLAnchorElement) => void;
+    initIndication: () => void;
+}
+
+const navDomApi: NavDomApi = {
+    setActive: function($clickedLink: HTMLAnchorElement): void {
+    let navLinks: NodeListOf<HTMLAnchorElement> =
+        document.querySelectorAll('.navbar-nav a');
+
+    navLinks.forEach(function($link) {
+        $link.classList.remove('text-primary');
+    });
+
+    $clickedLink.classList.add('text-primary');
+    },
+
+    initIndication: function(): void {
+        if ($mainLink instanceof HTMLAnchorElement) {
+            navDomApi.setActive($mainLink);
+            $mainLink.addEventListener('click', () => {
+                navDomApi.setActive($mainLink);
+            });
+        }
+        
+        if ($newExpenseLink instanceof HTMLAnchorElement) {
+            $newExpenseLink.addEventListener('click', () => {
+                navDomApi.setActive($newExpenseLink);
+            });
+        }
+        
+        if ($expensesListLink instanceof HTMLAnchorElement) {
+            $expensesListLink.addEventListener('click', () => {
+                navDomApi.setActive($expensesListLink);
+            });
+        }
+        if ($expenseForm instanceof HTMLAnchorElement) {
+            $expenseForm.addEventListener('submit', () => {
+                if ($expensesListLink instanceof HTMLAnchorElement) {
+                    navDomApi.setActive($expensesListLink);
+                }
+            });
+        }
+    }
+}
+
+/* Exports */
+
+export default navDomApi;
+
+
+
